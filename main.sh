7c2206927a4e7995e28d33d15405a8a8b75311e3
kilosecurity <dev@kilosecurity.net>
2026-01-04 16:28:32 +0100

    aggiunto script_dir

--- main.sh
+++ main.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ "$EUID" -ne 0 ]; then
     echo "Esegui come root"
     exit 1
 fi
 
-set -e
-./install_base.sh
-./install_mercure.sh
-./install_virtualhosts.sh
-./setup_project.sh
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+"$SCRIPT_DIR/install_base.sh"
+"$SCRIPT_DIR/install_mercure.sh"
+"$SCRIPT_DIR/install_virtualhosts.sh"
+"$SCRIPT_DIR/setup_project.sh"
