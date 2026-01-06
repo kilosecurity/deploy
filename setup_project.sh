fa24c4e3bb7d70fe132e0ee0504de5f471dfdf23
kilosecurity <dev@kilosecurity.net>
2026-01-06 21:55:55 +0100

    Add api project

--- setup_project.sh
+++ setup_project.sh
@@ -17,4 +17,15 @@ php artisan migrate --force
 chown -R www-data:www-data storage bootstrap/cache
 chmod -R 775 storage bootstrap/cache
 
+cd /var/www/api
+if [ -d "/var/www/api/.git" ]; then
+    echo ">>> Aggiorno la cartella api esistente"
+    git pull origin main
+else
+    echo ">>> Clono il repository da GitHub"
+    git clone git@github.com:kilosecurity/api.git /var/www/api
+fi
+composer install --no-dev --optimize-autoloader
+php artisan migrate --force
+
 echo ">>> Progetto configurato correttamente"
